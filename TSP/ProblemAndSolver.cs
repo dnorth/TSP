03d49dfc50ba9f8964474a70c92c9b46ce1de16d
tgrossen <tgrossen@gmail.com>
2015-04-10 02:58:35 -0600

    Everything through Thursday night

--- TSP/ProblemAndSolver.cs
+++ TSP/ProblemAndSolver.cs
@@ -3,9 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using Priority_Queue;
 
 namespace TSP
 {
+    public class State : PriorityQueueNode
+    {
+        public double[,] state;
+        public double lower_bound;
+        public double cost;
+        public int cities_left;
+        public int[,] route;
+        public State(double[,] state, double lower_bound, double cost, int cities_left, int[,] route)
+        {
+            this.state = state;
+            this.lower_bound = lower_bound;
+            this.cost = cost;
+            this.cities_left = cities_left;
+            this.route = route;
+        }
+        public State(double[,] state, double lower_bound)
+        {
+            this.state = state;
+            this.lower_bound = lower_bound;
+            this.cost = -1;
+            this.cities_left = -1;
+            this.route = null;
+        }
+    }
     class ProblemAndSolver
     {
         private class TSPSolution
@@ -242,6 +267,9 @@ namespace TSP
         double currCost;
         int minIndex;
         double[,] initialState;
+        HeapPriorityQueue<State> Agenda;
+        DateTime end;
+        State best_state;
 
         /// <summary>
         ///  solve the problem.  This is the entry point for the solver when the run button is clicked
@@ -249,18 +277,236 @@ namespace TSP
         /// </summary>
         public void solveProblem()
         {
+            DateTime start = DateTime.Now;
+            end = start.AddSeconds(60);
 
             /******* STEP 1 Create the initial State *******/
             init_state();
 
             /******* STEP 2 Get a 'Quick' BSSF *******/
+            quick_solution();
+            double init_bound = bound(initialState).lower_bound;
+            int[,] route = new int[Cities.Length, 2];
+            best_state = new State(initialState, init_bound, BSSF, Cities.Length - 1, route);
+
+            /******* STEP 3 Create a new Agenda *******/
+            // TODO: Agenda will be our Priority Queue (do we need to implement the multiplication thing when we add?? I dunno yet.)
+            Agenda = new HeapPriorityQueue<State>(100);
+            Agenda.Clear();
+            Agenda.Enqueue(best_state, init_bound);
+            while (Agenda.Count > 0 && DateTime.Now < end && BSSF != Agenda.First.lower_bound)
+            {
+                State u = Agenda.Dequeue();
+                if (u.lower_bound < BSSF)
+                {
+                    successors(u);
+                }
+            }
+
+            Route.Clear();
+
+            int[] ordered_route = get_ordered_route();
+
+            for (int i = 0; i < ordered_route.Length; i++)
+            {
+                Route.Add(Cities[ordered_route[i]]);
+            }
+
+            bssf = new TSPSolution(Route);
+
+            Program.MainForm.tbCostOfTour.Text = " " + BSSF;
+
+            Program.MainForm.tbElapsedTime.Text = " " + (DateTime.Now - start);
+
+            Program.MainForm.Invalidate();
+            /******* STEP 4 Add our first Route and BSSF to the agenda *******/
+            // TODO: We will add our state and the BSSF to the agenda
+
+            /******* STEP 5 Add our first Route and BSSF to the agenda *******/
+
+        }
+
+        // get_ordered_route() returns ordered route of cities after BSSF is determined
+        public int[] get_ordered_route()
+        {
+            int[] ordered_route = new int[Cities.Length];
+            int[,] state_route = best_state.route;
+
+            ordered_route[0] = state_route[0, 0];
+            ordered_route[1] = state_route[0, 1];
+            state_route[0, 0] = int.MaxValue;
+            state_route[0, 1] = int.MaxValue;
+
+            int current_city = ordered_route[1];
+
+            for (int i = 2; i < Cities.Length; i++)
+            {
+                int next_city = -1;
+                for (int x = 0; x < Cities.Length; x++)
+                {
+                    if (state_route[x, 0] == current_city) next_city = state_route[x, 1];
+                    else if (state_route[x, 1] == current_city) next_city = state_route[x, 0];
+                    if (next_city != -1)
+                    {
+                        state_route[x, 0] = int.MaxValue;
+                        state_route[x, 1] = int.MaxValue;
+                        break;
+                    }
+                }
+                ordered_route[i] = next_city;
+                current_city = next_city;
+            }
+            return ordered_route;
+        }
+
+        // heuristic(lower_bound, cities_left) evaluate heuristic function according to state
+        public double heuristic(double lower_bound, int cities_left)
+        {
+            return lower_bound + (cities_left * 7);
+        }
+
+        // criterion(state) evaluates state to determine if criterion or not
+        public bool criterion(double[,] state)
+        {
+            for (int x = 0; x < Cities.Length; x++)
+            {
+                for (int y = 0; y < Cities.Length; y++)
+                {
+                    if (state[x,y] != double.MaxValue)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        // evaluate_edge(i,j,state) decide whether to include/exclude, then add to agenda
+        public void evaluate_edge(int i, int j, double[,] state, int cities_left, int[,] route)
+        {
+            double[,] include = state;
+            double[,] exclude = state;
+            double[,] lowest_state = state;
+            exclude[i, j] = double.MaxValue;
+            for (int x = 0; x < Cities.Length; x++)
+            {
+                include[x, j] = double.MaxValue;
+                include[i, x] = double.MaxValue;
+            }
+            State exclude_state = bound(exclude);
+            State include_state = bound(exclude);
+            double exclude_bound = exclude_state.lower_bound;
+            double include_bound = include_state.lower_bound;
+            double lowest_bound = 0;
+            if (exclude_bound < include_bound)
+            {
+                lowest_bound = exclude_bound;
+                lowest_state = exclude_state.state;
+            }
+            else
+            {
+                lowest_bound = include_bound;
+                lowest_state = include_state.state;
+            }
+            double cost = heuristic(lowest_bound, cities_left-1);
+            if (DateTime.Now >= end)
+            {
+                return;
+            }
+            if (cost < BSSF)
+            {
+                int[,] new_route = route;
+                new_route[Cities.Length - 1 - cities_left, 0] = i;
+                new_route[Cities.Length - 1 - cities_left, 1] = j;
+                if (criterion(lowest_state))
+                {
+                    BSSF = cost;
+                    best_state = new State(lowest_state, lowest_bound, cost, cities_left - 1, new_route);
+                }
+                else
+                {
+                    Agenda.Enqueue(new State(lowest_state, lowest_bound, cost, cities_left - 1, new_route), cost);
+                }
+            }
+        }
+
+        // successors(State) find successors for given State
+        public void successors(State state)
+        {
+            int i = int.MaxValue;
+            int j = int.MaxValue;
+            for (int x = 0; x < Cities.Length; x++)
+            {
+                for (int y = 0; y < Cities.Length; y++)
+                {
+                    if (state.state[x,y] != double.MaxValue)
+                    {
+                        i = x;
+                        j = y;
+                        evaluate_edge(i, j, state.state, state.cities_left, state.route);
+                        return;
+                    }
+                }
+            }
+        }
+
+        // bound(costMatrix) find lower bound for given cost matrix
+        public State bound(double[,] state)
+        {
+            double lower_bound = 0;
+            // reduce by row
+            for (int x = 0; x < Cities.Length; x++)
+            {
+                double lowest = double.MaxValue;
+                for (int y = 0; y < Cities.Length; y++)
+                {
+                    if (state[x,y] < lowest)
+                    {
+                        lowest = state[x,y];
+                    }
+                }
+                if (lowest != 0 && lowest != double.MaxValue)
+                {
+                    lower_bound += lowest;
+                    for (int y = 0; y < Cities.Length; y++)
+                    {
+                        state[x,y] -= lowest;
+                    }
+                }
+            }
+            // reduce by column
+            for (int y = 0; y < Cities.Length; y++)
+            {
+                double lowest = double.MaxValue;
+                for (int x = 0; x < Cities.Length; x++)
+                {
+                    if (state[x,y] < lowest)
+                    {
+                        lowest = state[x, y];
+                    }
+                }
+                if (lowest != 0 && lowest != double.MaxValue)
+                {
+                    lower_bound += lowest;
+                    for (int x = 0; x < Cities.Length; x++)
+                    {
+                        state[x, y] -= lowest;
+                    }
+                }
+            }
+            return new State(state, lower_bound);
+        }
+
+        // quick_solution, not using initial state cost matrix
+        public void quick_solution()
+        {
             int x;
             int currIndex = 0;
             Route = new ArrayList();
 
             //Get the next neighbor greedily based on lowest cost
             //While our Route list size is less than the number of cities (meaning we finish once we get to all the cities)
-            while(Route.Count < Cities.Length)
+            while (Route.Count < Cities.Length)
             {
                 //Get an arbitrarily large minimum cost
                 minCost = double.MaxValue;
@@ -269,17 +515,17 @@ namespace TSP
                 for (x = 0; x < Cities.Length; x++)
                 {
                     //Make sure we're not trying to check against ourself, because that would definitely be the minimum cost and kind of ruin this whole thing.....
-                    if(x != currIndex)
+                    if (x != currIndex)
                     {
                         //Make sure the Route list doesn't already contain the city we're trying to add. If it does, it won't be a tour so we don't want to use it again
-                        if(!Route.Contains(Cities[x]))
+                        if (!Route.Contains(Cities[x]))
                         {
                             //Get the cost from the current index to the next index. If it's less than what we have, make the 'minCost' equal to our 'currCost' and choose our new minimum index
                             currCost = Cities[currIndex].costToGetTo(Cities[x]);
-                            if(currCost < minCost)
+                            if (currCost < minCost)
                             {
                                 minCost = currCost;
-                                minIndex = currIndex;
+                                minIndex = x;
                             }
                         }
                     }
@@ -292,15 +538,17 @@ namespace TSP
             //Once we have a complete tour, we need to calculate the cost of it and make it our initial BSSF
             bssf = new TSPSolution(Route);
             BSSF = bssf.costOfRoute();
+        }
 
-            /******* STEP 3 Create a new Agenda *******/
-            // TODO: Agenda will be our Priority Queue (do we need to implement the multiplication thing when we add?? I dunno yet.)
-
-            /******* STEP 4 Add our first Route and BSSF to the agenda *******/
-            // TODO: We will add our state and the BSSF to the agenda
-
-            /******* STEP 5 Add our first Route and BSSF to the agenda *******/
-
+        // return factorial of given number
+        public int factorial(int number)
+        {
+            int fact = number;
+            for (int x = 1; x < number; x++)
+            {
+                fact *= x;
+            }
+            return fact;
         }
 
         public void init_state()
@@ -308,7 +556,7 @@ namespace TSP
             initialState = new double[Cities.Length, Cities.Length];
             for(int i = 0; i < Cities.Length; i++)
             {
-                for (int j = 0; j < Cities.Length; i++)
+                for (int j = 0; j < Cities.Length; j++)
                 {
                     if(i == j)
                     {
